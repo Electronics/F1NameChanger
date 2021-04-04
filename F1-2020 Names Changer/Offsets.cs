0cc228f8aba53f55918cc73d73d50a3decb2b64f
Laurie Kirkcaldy <lauriekirkcaldy@gmail.com>
2021-04-04 20:18:02 +0100

    Forgot these files when adding DX11 support

---
+++ F1-2020 Names Changer/Offsets.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1_2020_Names_Changer {
+	static class Offsets {
+        // these are addresses at which this program will start to search for the below search strings to identify the where the actual start of the region is
+        public static IntPtr MENU_OFFSET_START;
+        public static IntPtr MENU2_OFFSET_START;
+        public static IntPtr CHARSELECTION_OFFSET_START;
+        public static IntPtr INGAME_OFFSET_START;
+
+        // teams offset separately stated as there doesn't seem to be much order to how they're organised. Plus the memory locations are static
+        public static IntPtr TEAMS_OFFSET_MENU_RACING_POINT;
+        public static IntPtr TEAMS_OFFSET_MENU_MERCEDES;
+        public static IntPtr TEAMS_OFFSET_MENU_FERRARI;
+        public static IntPtr TEAMS_OFFSET_MENU_RED_BULL;
+        public static IntPtr TEAMS_OFFSET_MENU_ALPHA_TAURI;
+        public static IntPtr TEAMS_OFFSET_MENU_RENAULT;
+        public static IntPtr TEAMS_OFFSET_MENU_ALFA_ROMEO;
+        public static IntPtr TEAMS_OFFSET_MENU_WILLIAMS;
+        public static IntPtr TEAMS_OFFSET_MENU_HAAS;
+        public static IntPtr TEAMS_OFFSET_MENU_MCLAREN;
+
+        public static IntPtr TEAMS_OFFSET_GAME_RACING_POINT;
+        public static IntPtr TEAMS_OFFSET_GAME_MERCEDES;
+        public static IntPtr TEAMS_OFFSET_GAME_FERRARI;
+        public static IntPtr TEAMS_OFFSET_GAME_RED_BULL;
+        public static IntPtr TEAMS_OFFSET_GAME_ALPHA_TAURI;
+        public static IntPtr TEAMS_OFFSET_GAME_RENAULT;
+        public static IntPtr TEAMS_OFFSET_GAME_ALFA_ROMEO;
+        public static IntPtr TEAMS_OFFSET_GAME_WILLIAMS;
+        public static IntPtr TEAMS_OFFSET_GAME_HAAS;
+        public static IntPtr TEAMS_OFFSET_GAME_MCLAREN;
+
+        public static void loadDX12() {
+            MENU_OFFSET_START = (IntPtr)0x2b1d12715;
+            MENU2_OFFSET_START = (IntPtr)0x2b1f83800;
+            CHARSELECTION_OFFSET_START = (IntPtr)0x2b1b5760f;
+            INGAME_OFFSET_START = (IntPtr)0x2b18f5000;
+            TEAMS_OFFSET_MENU_RACING_POINT = (IntPtr)0x1942e67cd;
+            TEAMS_OFFSET_MENU_MERCEDES = (IntPtr)0x194350b33;
+            TEAMS_OFFSET_MENU_FERRARI = (IntPtr)0x194350be5;
+            TEAMS_OFFSET_MENU_RED_BULL = (IntPtr)0x194350db2;
+            TEAMS_OFFSET_MENU_ALPHA_TAURI = (IntPtr)0x194350f1b;
+            TEAMS_OFFSET_MENU_RENAULT = (IntPtr)0x19435100f;
+            TEAMS_OFFSET_MENU_ALFA_ROMEO = (IntPtr)0x194351164;
+            TEAMS_OFFSET_MENU_WILLIAMS = (IntPtr)0x194351248;
+            TEAMS_OFFSET_MENU_HAAS = (IntPtr)0x19435141d;
+            TEAMS_OFFSET_MENU_MCLAREN = (IntPtr)0x19435131c;
+            TEAMS_OFFSET_GAME_RACING_POINT = (IntPtr)0x1942e6d54;
+            TEAMS_OFFSET_GAME_MERCEDES = (IntPtr)0x194351232;
+            TEAMS_OFFSET_GAME_FERRARI = (IntPtr)0x194350df6;
+            TEAMS_OFFSET_GAME_RED_BULL = (IntPtr)0x194350a80;
+            TEAMS_OFFSET_GAME_ALPHA_TAURI = (IntPtr)0x194350cf5;
+            TEAMS_OFFSET_GAME_RENAULT = (IntPtr)0x194350e69;
+            TEAMS_OFFSET_GAME_ALFA_ROMEO = (IntPtr)0x194350f84;
+            TEAMS_OFFSET_GAME_WILLIAMS = (IntPtr)0x194350ab5;
+            TEAMS_OFFSET_GAME_HAAS = (IntPtr)0x194350b09;
+            TEAMS_OFFSET_GAME_MCLAREN = (IntPtr)0x194351273;
+        }
+
+        public static void loadDX11() {
+            MENU_OFFSET_START = (IntPtr)0x2b13d2000;
+            MENU2_OFFSET_START = (IntPtr)0x2b1643200;
+            CHARSELECTION_OFFSET_START = (IntPtr)0x2b1217100;
+            INGAME_OFFSET_START = (IntPtr)0x2b0fb6900;
+            TEAMS_OFFSET_MENU_RACING_POINT = (IntPtr)0x1939a67cd;
+            TEAMS_OFFSET_MENU_MERCEDES = (IntPtr)0x193a10b33;
+            TEAMS_OFFSET_MENU_FERRARI = (IntPtr)0x193a10be5;
+            TEAMS_OFFSET_MENU_RED_BULL = (IntPtr)0x193a10db2;
+            TEAMS_OFFSET_MENU_ALPHA_TAURI = (IntPtr)0x193a10f1b;
+            TEAMS_OFFSET_MENU_RENAULT = (IntPtr)0x193a1100f;
+            TEAMS_OFFSET_MENU_ALFA_ROMEO = (IntPtr)0x193a11164;
+            TEAMS_OFFSET_MENU_WILLIAMS = (IntPtr)0x193a11248;
+            TEAMS_OFFSET_MENU_HAAS = (IntPtr)0x193a1141d;
+            TEAMS_OFFSET_MENU_MCLAREN = (IntPtr)0x193a1131c;
+            TEAMS_OFFSET_GAME_RACING_POINT = (IntPtr)0x1939a6d54;
+            TEAMS_OFFSET_GAME_MERCEDES = (IntPtr)0x193a11232;
+            TEAMS_OFFSET_GAME_FERRARI = (IntPtr)0x193a10df6;
+            TEAMS_OFFSET_GAME_RED_BULL = (IntPtr)0x193a10a80;
+            TEAMS_OFFSET_GAME_ALPHA_TAURI = (IntPtr)0x193a10cf5;
+            TEAMS_OFFSET_GAME_RENAULT = (IntPtr)0x193a10e69;
+            TEAMS_OFFSET_GAME_ALFA_ROMEO = (IntPtr)0x193a10f84;
+            TEAMS_OFFSET_GAME_WILLIAMS = (IntPtr)0x193a10ab5;
+            TEAMS_OFFSET_GAME_HAAS = (IntPtr)0x193a10b09;
+            TEAMS_OFFSET_GAME_MCLAREN = (IntPtr)0x193a11273;
+        }
+    }
+}
---
+++ Reverse Engineering/FindingNewOffsets.md
@@ -0,0 +1,12 @@
+Procedure for finding new offsets:
+
+(All offsets we care about will be >0x2b000000, stuff below tends to be history/menu cruft)
+
+All found offsets, subtract off ~0xb00 for randomisation.
+
+MENU_OFFSET_START: Look for `{o:mixed}Carlos`
+MENU2_OFFSET_START: Look for `{o:mixed}Daniel` (should be ~ 0x27100 above MENU_OFFSET_START)
+CHARSELECTION_OFFSET_START: Look for `Carlos SAINZ`, looking for a region with drivers ending with `.DRIVER].bk2` (or some truncation of)
+INGAME_OFFSET_START: Look for `Carlos`, looking for region with 32-byte aligned rows of Firstname, Lastname, Tag
+TEAMS_OFFSET_MENU_*: look for `Chief Technical Officer` or `{o:upper}M{/o}{o:lower}c{/o}{o:upper}L{/o}aren F1 Team`, it should be around this region. Have to manually go through and find each offset, trying each time as there are duplicates which the game doesn't use. It seems to almost always be the address just before 0x20000000.
+TEAMS_OFFSET_GAME_*: look for `Mercedes-AMG Petronas` nearest 0x20000000 (should be around TEAMS_OFFSET_MENU stuff), again manual finding
